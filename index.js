48c53f63c1978bd6e6d798223f4fffffaa2cbdd1
Marcelo <sl_marcelo@hotmail.com>
2024-11-12 17:26:55 -0300

    update

--- index.js
+++ index.js
@@ -10,33 +10,38 @@ function heroi(nomeHeroi, xpHeroi) {
         categporia: ""
     }
 
-    if (xpHeroi > 0 && xpHeroi < 1000) {
-
+    if (xpHeroi >= 0 && xpHeroi <= 1000) {
         heroiDados.nome = nomeHeroi, heroiDados.nivel = xpHeroi, heroiDados.categporia = "Ferro"
-
         console.log(`O Herói de nome ${heroiDados.nome} está no nível de ${heroiDados.nivel} xp e sua categoria é ${heroiDados.categporia}`)
+
     } else if (xpHeroi > 1000 && xpHeroi <= 2000) {
         heroiDados.nome = nomeHeroi, heroiDados.nivel = xpHeroi, heroiDados.categporia = "Bronze"
         console.log(`O Herói de nome ${heroiDados.nome} está no nível de ${heroiDados.nivel} xp e sua categoria é ${heroiDados.categporia}`)
+
     } else if (xpHeroi > 2000 && xpHeroi <= 5000) {
         heroiDados.nome = nomeHeroi, heroiDados.nivel = xpHeroi, heroiDados.categporia = "Prata"
         console.log(`O Herói de nome ${heroiDados.nome} está no nível de ${heroiDados.nivel} xp e sua categoria é ${heroiDados.categporia}`)
+
     } else if (xpHeroi > 5000 && xpHeroi <= 7000) {
         heroiDados.nome = nomeHeroi, heroiDados.nivel = xpHeroi, heroiDados.categporia = "Ouro"
         console.log(`O Herói de nome ${heroiDados.nome} está no nível de ${heroiDados.nivel} xp e sua categoria é ${heroiDados.categporia}`)
+
     } else if (xpHeroi > 7000 && xpHeroi <= 8000) {
         heroiDados.nome = nomeHeroi, heroiDados.nivel = xpHeroi, heroiDados.categporia = "Platina"
         console.log(`O Herói de nome ${heroiDados.nome} está no nível de ${heroiDados.nivel} xp e sua categoria é ${heroiDados.categporia}`)
+
     } else if (xpHeroi > 8000 && xpHeroi <= 9000) {
         heroiDados.nome = nomeHeroi, heroiDados.nivel = xpHeroi, heroiDados.categporia = "Ascendente"
         console.log(`O Herói de nome ${heroiDados.nome} está no nível de ${heroiDados.nivel} xp e sua categoria é ${heroiDados.categporia}`)
+
     } else if (xpHeroi > 9000 && xpHeroi <= 10000) {
         heroiDados.nome = nomeHeroi, heroiDados.nivel = xpHeroi, heroiDados.categporia = "Imortal"
         console.log(`O Herói de nome ${heroiDados.nome} está no nível de ${heroiDados.nivel} xp e sua categoria é ${heroiDados.categporia}`)
     } else if (xpHeroi > 10000) {
         heroiDados.nome = nomeHeroi, heroiDados.nivel = xpHeroi, heroiDados.categporia = "Radiante"
         console.log(`O Herói de nome ${heroiDados.nome} está no nível de ${heroiDados.nivel} xp e sua categoria é ${heroiDados.categporia}`)
-    } else {
+
+    } else if (xpHeroi < 0) {
         heroiDados.nome = nomeHeroi, heroiDados.nivel = xpHeroi, heroiDados.categporia = "Inválida"
         console.log(`O herói chamado ${heroiDados.nome} com valor de ${heroiDados.nivel} inválido. Coloque somente números positivos.`)
     }
@@ -44,8 +49,8 @@ function heroi(nomeHeroi, xpHeroi) {
 }
 
 
-heroi("Ferro", 800)
-heroi("Bronze", 1500)
+ heroi("Ferro", 800)
+heroi("Bronze", 1001)
 heroi("Prata", 2500)
 heroi("Ouro", 5500)
 heroi("Platina", 7500)
